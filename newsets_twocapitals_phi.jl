5a24caeb9467c485bc5cc9c671e9744dfe8cd32f
Haomin Qin <hqin@midway3-login4.rcc.local>
2023-03-08 09:42:36 -0600

    1

---
+++ newsets_twocapitals_phi.jl
@@ -0,0 +1,249 @@
+#=============================================================================#
+#  Economy with TWO CAPITAL STOCKS
+#
+#  Author: Balint Szoke
+#  Date: Sep 2018
+#=============================================================================#
+
+using Pkg
+using Optim
+using Roots
+using NPZ
+using Distributed
+using CSV
+using Tables
+using ArgParse
+
+function parse_commandline()
+    s = ArgParseSettings()
+    @add_arg_table s begin
+        "--gamma"
+            help = "gamma"
+            arg_type = Float64
+            default = 8.0
+        "--rho"
+            help = "rho"
+            arg_type = Float64
+            default = 1.00001   
+        "--kappa"
+            help = "kappa"
+            arg_type = Float64
+            default = 0.0
+        "--zeta"
+            help = "zeta"
+            arg_type = Float64
+            default = 0.5  
+        "--fraction"
+            help = "fraction"
+            arg_type = Float64
+            default = 0.01   
+        "--Delta"
+            help = "Delta"
+            arg_type = Float64
+            default = 1000.  
+        "--symmetric"
+            help = "symmetric"
+            arg_type = Int
+            default = 0
+        "--dataname"
+            help = "dataname"
+            arg_type = String
+            default = "output"
+    end
+    return parse_args(s)
+end
+
+#==============================================================================#
+# SPECIFICATION:
+#==============================================================================#
+@show parsed_args = parse_commandline()
+gamma                = parsed_args["gamma"]
+rho                  = parsed_args["rho"]
+kappa                = parsed_args["kappa"]
+zeta                 = parsed_args["zeta"]
+fraction             = parsed_args["fraction"]
+Delta                = parsed_args["Delta"]
+symmetric            = parsed_args["symmetric"]
+dataname             = parsed_args["dataname"]
+
+symmetric_returns    = symmetric
+state_dependent_xi   = 0
+optimize_over_ell    = 0
+compute_irfs         = 0                    # need to start julia with "-p 5"
+
+if compute_irfs == 1
+    @everywhere include("newsets_utils_phi.jl")
+elseif compute_irfs ==0
+    include("newsets_utils_phi.jl")
+end
+
+println("=============================================================")
+if symmetric_returns == 1
+    println(" Economy with two capital stocks: SYMMETRIC RETURNS          ")
+    if state_dependent_xi == 0
+        println(" No tilting (xi is NOT state dependent)                      ")
+        filename = (compute_irfs==0) ? "model_sym_HS.npz" : "model_sym_HS_p.npz";
+    elseif state_dependent_xi == 1
+        println(" With tilting (change in kappa)                        ")
+        filename = (compute_irfs==0) ? "model_sym_HSHS.npz" : "model_sym_HSHS_p.npz";
+    elseif state_dependent_xi == 2
+        println(" With tilting (change in beta)                        ")
+        filename = (compute_irfs==0) ? "model_sym_HSHS2.npz" : "model_sym_HSHS2_p.npz";
+    end
+elseif symmetric_returns == 0
+    println(" Economy with two capital stocks: ASYMMETRIC RETURNS         ")
+    if state_dependent_xi == 0
+        println(" No tilting (xi is NOT state dependent)                      ")
+        filename = (compute_irfs==0) ? "model_asym_HS.npz" : "model_asym_HS_p.npz";
+    elseif state_dependent_xi == 1
+        println(" With tilting (change in kappa)                        ")
+        filename = (compute_irfs==0) ? "model_asym_HSHS.npz" : "model_asym_HSHS_p.npz";
+    elseif state_dependent_xi == 2
+        println(" With tilting (change in beta)                        ")
+        filename = (compute_irfs==0) ? "model_asym_HSHS2.npz" : "model_asym_HSHS2_p.npz";
+    end
+end
+
+filename_ell = "./output/"*dataname*"/kappa_"*string(kappa)*"_zeta_"*string(zeta)*"/gamma_"*string(gamma)*"_rho_"*string(rho)*"/"
+isdir(filename_ell) || mkpath(filename_ell)
+
+#==============================================================================#
+#  PARAMETERS
+#==============================================================================#
+
+# (1) Baseline model
+a11 = 0.014
+alpha = 0.1
+
+scale = 1.32
+sigma_k1 = scale*[.0048,               .0,   .0];
+sigma_k2 = scale*[.0              , .0048,   .0];
+
+scale = sqrt(1.754)
+sigma_k1 = scale*[.00477,               .0,   .0];
+sigma_k2 = scale*[.0              , .00477,   .0];
+sigma_z =  [.011*sqrt(.5)   , .011*sqrt(.5)   , .025];
+
+eta1 = 0.013
+eta2 = 0.013
+eta1 = 0.012790328319261378
+eta2 = 0.012790328319261378
+if symmetric_returns == 1
+    beta1 = 0.01
+    beta2 = 0.01
+    beta1 = 0.005
+    beta2 = 0.005
+else
+    beta1 = 0.0
+    beta2 = 0.01
+    beta1 = 0.0
+    beta2 = 0.005
+end
+
+delta = .002;
+
+phi1 = 28.0
+phi2 = 28.0
+
+# (3) GRID
+II, JJ = 1001, 201;
+rmax = 1.;#log(20);
+rmin = -1.;#-log(20); 
+# rmax = 3.;#log(20);
+# rmin = -3.;#-log(20); 
+zmax = 1.;
+# zmax = 0.5;
+zmin = -zmax;
+
+# (4) Iteration parameters
+maxit = 50000;        # maximum number of iterations in the HJB loop
+# maxit = 6;        # maximum number of iterations in the HJB loop
+crit  = 10e-6;      # criterion HJB loop
+# Delta = 1000.;      # delta in HJB algorithm
+
+
+# Initialize model objects -----------------------------------------------------
+baseline1 = Baseline(a11, zeta, kappa, sigma_z, beta1, eta1, sigma_k1, delta);
+baseline2 = Baseline(a11, zeta, kappa, sigma_z, beta2, eta2, sigma_k2, delta);
+technology1 = Technology(alpha, phi1);
+technology2 = Technology(alpha, phi2);
+model = TwoCapitalEconomy(baseline1, baseline2, technology1, technology2);
+
+grid = Grid_rz(rmin, rmax, II, zmin, zmax, JJ);
+params = FinDiffMethod(maxit, crit, Delta);
+
+#==============================================================================#
+# WITH ROBUSTNESS
+#==============================================================================#
+
+println(" (3) Compute value function WITH ROBUSTNESS")
+times = @elapsed begin
+A, V, val, d1_F, d2_F, d1_B, d2_B, h1_F, h2_F, hz_F, h1_B, h2_B, hz_B,
+        mu_1_F, mu_1_B, mu_r_F, mu_r_B, mu_z, V0, Vr, Vz, rr, zz, pii, dr, dz =
+        value_function_twocapitals(gamma, rho, fraction, model, grid, params, symmetric_returns);
+println("=============================================================")
+end
+println("Convegence time (minutes): ", times/60)
+g_dist, g = stationary_distribution(A, grid)
+
+# Define Policies object
+policies  = PolicyFunctions(d1_F, d2_F, d1_B, d2_B,
+                            -h1_F, -h2_F, -hz_F,
+                            -h1_B, -h2_B, -hz_B);
+
+# Construct drift terms under the baseline
+mu_1 = (mu_1_F + mu_1_B)/2.;
+mu_r = (mu_r_F + mu_r_B)/2.;
+# ... under the worst-case model
+h1_dist = (policies.h1_F + policies.h1_B)/2.;
+h2_dist = (policies.h2_F + policies.h2_B)/2.;
+hz_dist = (policies.hz_F + policies.hz_B)/2.;
+
+######
+d1 = (policies.d1_F + policies.d1_B)/2;
+d2 = (policies.d2_F + policies.d2_B)/2;
+h1, h2, hz = -h1_dist, -h2_dist, -hz_dist;
+
+
+r = range(rmin, stop=rmax, length=II);    # capital ratio vector
+rr = r * ones(1, JJ);
+pii = rr;
+IJ = II*JJ;
+k1a = zeros(II,JJ)
+k2a = zeros(II,JJ)
+for i=1:IJ
+    p = pii[i];
+    k1a[i] = (1-zeta + zeta*exp.(p*(1-kappa))).^(1/(kappa-1));
+    k2a[i] = ((1-zeta)*exp.(p*((kappa-1))) + zeta).^(1/(kappa-1));
+end
+d1k = d1.*k1a
+d2k = d2.*k2a
+c = alpha*ones(II,JJ) - d1k - d2k
+# CSV.write(filename_ell*"d1.csv",  Tables.table(d1), writeheader=false)
+# CSV.write(filename_ell*"d2.csv",  Tables.table(d2), writeheader=false)
+# CSV.write(filename_ell*"h1.csv",  Tables.table(h1), writeheader=false)
+# CSV.write(filename_ell*"h2.csv",  Tables.table(h2), writeheader=false)
+# CSV.write(filename_ell*"hz.csv",  Tables.table(hz), writeheader=false)
+
+results = Dict("delta" => delta,
+# Two capital stocks
+"eta1" => eta1, "eta2" => eta2, "a11"=> a11, 
+"beta1" => beta1, "beta2" => beta2,
+"k1a" => k1a, "k2a"=> k2a,
+"d1k" => d1k, "d2k"=> d2k,
+"sigma_k1" => sigma_k1, "sigma_k2" => sigma_k2,
+"sigma_z" =>  sigma_z, "alpha" => alpha, "kappa" => kappa,"zeta" => zeta, "phi1" => phi1, "phi2" => phi2,
+"I" => II, "J" => JJ,
+"rmax" => rmax, "rmin" => rmin, "zmax" => zmax, "zmin" => zmin,
+"rr" => rr, "zz" => zz, "pii" => pii, "dr" => dr, "dz" => dz,
+"maxit" => maxit, "crit" => crit, "Delta" => Delta,
+"g_dist" => g_dist, "g" => g,
+"cons" => c,
+# Robust control under baseline
+"V0" => V0, "V" => V, "Vr" => Vr, "Vz" => Vz, "val" => val, "gamma" => gamma, "rho" => rho,
+"d1" => d1, "d2" => d2,
+"h1" => h1, "h2" => h2, "hz" => hz,
+"times" => times,
+"mu_1" => mu_1, "mu_r" => mu_r, "mu_z" => mu_z)
+
+npzwrite(filename_ell*filename, results)
